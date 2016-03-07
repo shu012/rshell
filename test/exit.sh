205a2168275256bf463ee875574eb3c4e01746bd
Sara Hu <shu012@ucr.edu>
2016-03-06 18:11:52 -0800

    new exit test script

--- test/exit.sh
+++ test/exit.sh
@@ -2,8 +2,23 @@
 
 make -C ..
 ./../a.out << 'EOF'
+mkdir test
+test -e test
+[ -e test ]
+test -e test && ls
+[ -e test ] && ls
+exit
+EOF
+
+make -C ..
+./../a.out << 'EOF'
+echo hello
+echo checking exit
 exit
 EOF
 
 printf "\n"
+ls
+rm -rf test
+
 
